c753f21a17113d34c7996770e327fbffb4a53f13
aiangelov127 <aiangelov127@gmail.com>
2021-10-12 17:28:29 +0300

    Edit3
    Edit3

--- Withdraw_Deposit.py
+++ Withdraw_Deposit.py
@@ -4,24 +4,30 @@ today = date.today()
 
 class Account:
     
-    def __init__(self, account_id = str(input("what is your ID? ")), balance = 0.00):        
+    def __init__(self, balance = 0.00, account_id = str(input("what is your ID? "))):        
         self.balance = balance
         self.account_id = account_id
-    
+
+    def get_balance(self):
+        with open(f'{self.account_id}_account.txt', 'r') as file:
+            self.balance = float(file.readlines()[-1])       
+
     def get_user_id(self):
         with open('Accounts.txt', 'r') as file:
             accounts_list = file.read()
-            list = accounts_list.split(', ')
+            list = accounts_list.split('\n')
             user_list = list
-        for num in user_list:
-            if num == self.account_id:
+            print(user_list)
+            if self.account_id in user_list:
                 print(f"Your ID is {self.account_id}")
-                break
             else:
+                print("No such user.")
                 with open('Accounts.txt', 'a') as file:
-                    file.write(f", {self.account_id}")
+                    file.write(f"\n{self.account_id}")
+                with open(f'{self.account_id}_account.txt', 'w') as file:
+                    file.write("0.00")
                 print(f"Your account ID {self.account_id} was added.")
-                break
+                
 
     def log_transaction(self):
         with open(f'{self.account_id}_account.txt', 'a') as file:
@@ -37,12 +43,14 @@ class Account:
 account = Account() 
 
 while True:
+    
     account.get_user_id()
+    account.get_balance()
 
     user_input = input("What do you want to do: deposit, withdraw or exit? ")
 
     if user_input == 'deposit':
-        money = float(input("How much money do you want to deposit?"))
+        money = float(input("How much? "))
         account.deposit(money)
         account.log_transaction()
         print(f"You deposited ${money}. Your current balance is ${account.balance}")
